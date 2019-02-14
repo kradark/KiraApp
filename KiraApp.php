bdffec592a82e1899b60ffdb7e8b9983894528f6
kradark <kradark@gmail.com>
2019-02-14 14:40:24 +0800

    Update KiraApp.php

--- KiraApp.php
+++ KiraApp.php
@@ -8,166 +8,53 @@
   	$sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
   	$sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
   	$sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
-  	$msg_json = '{
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "action": {
-			"type": "uri",
-			"uri": "http://linecorp.com/"
-		  }
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "contents": [
-			{
-			  "type": "text",
-			  "text": "Brown Cafe",
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "margin": "md",
-			  "contents": [
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-				},
-				{
-				  "type": "icon",
-				  "size": "sm",
-				  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-				},
-				{
-				  "type": "text",
-				  "text": "4.0",
-				  "size": "sm",
-				  "color": "#999999",
-				  "margin": "md",
-				  "flex": 0
-				}
-			  ]
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "margin": "lg",
-			  "spacing": "sm",
-			  "contents": [
-				{
-				  "type": "box",
-				  "layout": "baseline",
-				  "spacing": "sm",
-				  "contents": [
-					{
-					  "type": "text",
-					  "text": "Place",
-					  "color": "#aaaaaa",
-					  "size": "sm",
-					  "flex": 1
-					},
-					{
-					  "type": "text",
-					  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-					  "wrap": true,
-					  "color": "#666666",
-					  "size": "sm",
-					  "flex": 5
-					}
-				  ]
-				},
-				{
-				  "type": "box",
-				  "layout": "baseline",
-				  "spacing": "sm",
-				  "contents": [
-					{
-					  "type": "text",
-					  "text": "Time",
-					  "color": "#aaaaaa",
-					  "size": "sm",
-					  "flex": 1
-					},
-					{
-					  "type": "text",
-					  "text": "10:00 - 23:00",
-					  "wrap": true,
-					  "color": "#666666",
-					  "size": "sm",
-					  "flex": 5
-					}
-				  ]
-				}
-			  ]
-			}
-		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "style": "link",
-			  "height": "sm",
-			  "action": {
-				"type": "uri",
-				"label": "CALL",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "style": "link",
-			  "height": "sm",
-			  "action": {
-				"type": "uri",
-				"label": "WEBSITE",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "spacer",
-			  "size": "sm"
-			}
-		  ],
-		  "flex": 0
-		}
-	  }';
-  	$response = array (
+	  
+	$response = array (
 		"replyToken" => $sender_replyToken,
 		"messages" => array (
 			array (
-				"type" => "flex",
-				"altText" => "This is a Flex Message",
-				"contents" => json_decode($msg_json)
+				"type" => "text",
+				"text" => "請試用quick replies功能",
+				"quickReply" => array (
+					"items" => array (
+						array (
+							"type" => "action",
+							"imageUrl" => "https://www.google.com.tw/logos/doodles/2019/valentines-day-2019-4848332248711168-s.png",
+							"action" => array (
+								"type" => "message",
+								"label"=> "Apple",
+								"text" => "這是一個Apple"
+							)
+						),
+						array (
+                            "type" => "action",
+                            "imageUrl" => "https://pic.pimg.tw/dreammaker88/1443152599-1854556715.png",
+                            "action" => array (
+                                "type" => "location",
+                                "label"=> "請選擇位置"
+                            )
+                        ),
+                        array (
+                            "type" => "action",
+                            "imageUrl" => "https://pic.pimg.tw/dreammaker88/1443152599-1854556715.png",
+                            "action" => array (
+                                "type" => "camera",
+                                "label"=> "啟動相機"
+                            )
+                        ),
+                        array (
+                            "type" => "action",
+                            "imageUrl" => "https://pic.pimg.tw/dreammaker88/1443152599-1854556715.png",
+                            "action" => array (
+                                "type" => "cameraRoll",
+                                "label"=> "啟動相簿"
+                            )
+						)
+					)
+				)	
 			)
 		)
-  	);
+	);
 			
   	fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   	$header[] = "Content-Type: application/json";
@@ -180,4 +67,3 @@ $header[] = "Authorization: Bearer 58tGd62pBsrYGL7qy1kx+LJCG8W/SheF6lG0CsIJuP0Re
   	$result = curl_exec($ch);
   	curl_close($ch);
 ?>
-
