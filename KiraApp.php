cb964d2c327b1ea3fddf97742a07dda6fd554974
kradark <kradark@gmail.com>
2019-02-14 11:56:39 +0800

    Update KiraApp.php

--- KiraApp.php
+++ KiraApp.php
@@ -7,8 +7,10 @@
   
     $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
     $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
-  
+    $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
+
     $response = array (
+        "replyToken" => $sender_replyToken,
         "to" => $sender_userid,
         "messages" => array (
             array (
@@ -21,7 +23,7 @@
     fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
     $header[] = "Content-Type: application/json";
     $header[] = "Authorization: Bearer 58tGd62pBsrYGL7qy1kx+LJCG8W/SheF6lG0CsIJuP0Rerj/i6md02bTC7ipkRtCC9epuOdT1LVE+gtfk0QD74eA6qJ6nfk9A4UeS8alVgrFkL+2Ww7ZcWzgcFN90KuXkLJ9n6iXKEmFIGPItm4iBwdB04t89/1O/w1cDnyilFU=";
-    $ch = curl_init("https://api.line.me/v2/bot/message/push");
+    $ch = curl_init("https://api.line.me/v2/bot/message/reply");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
