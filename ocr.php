384faec387203e3d8f720d19071ef44a6c964b6f
kradark <kradark@gmail.com>
2019-03-05 16:07:33 +0800

    Update ocr.php

--- ocr.php
+++ ocr.php
@@ -66,8 +66,8 @@
         "messages" => array (
             array (
    	"type" => "image",
-              "originalContentUrl" => "https://159.65.4.103/cht20190214/kira/9460295536836.jpeg",
-		"previewImageUrl" => "https://159.65.4.103/cht20190214/kira/9460301445472.jpeg"    
+              "originalContentUrl" => "https://159.65.4.103/cht20190214/kira/".$imageId.".jpeg",
+		"previewImageUrl" => "https://txmn.org/files/2014/10/dragonfly-black-and-white.jpg"    
 	    
    //             "type" => "text",
      //           "text" => $ans_txt.$ans_txt1
